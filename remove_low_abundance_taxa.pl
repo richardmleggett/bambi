cc8e930dbc88510d1543d22374398ff788c271a9
Richard Leggett <richard.leggett@earlham.ac.uk>
2017-06-07 18:11:14 +0100

    Update

--- remove_low_abundance_taxa.pl
+++ remove_low_abundance_taxa.pl
@@ -16,7 +16,7 @@ print OUTFILE $line;
 while (<INFILE>) {
     my $line = $_;
     my @arr = split(/\t/, $line);
-    my $total = $arr[1] + $arr[2] + $arr[3] + $arr[4] + $arr[5] + $arr[6] + $arr[7] + $arr[8];
+    my $total = $arr[1] + $arr[2] + $arr[3] + $arr[4] + $arr[5] + $arr[6];
     if ($total >= $threshold) {
         print OUTFILE $line;
     }
